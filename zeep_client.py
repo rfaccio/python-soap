cde9bff67ac0d7bd5dbe08fbe873409b3933daf3
Rafael <faccio.rafael@gmail.com>
2019-10-30 15:55:19 -0300

    removes unused variables

--- zeep_client.py
+++ zeep_client.py
@@ -2,19 +2,16 @@ import json
 import operator
 
 import zeep
-from zeep.wsse.username import UsernameToken
-from zeep.transports import Transport
-
-from requests.auth import HTTPBasicAuth  # or HTTPDigestAuth, or OAuth1, etc.
 from requests import Session
+from requests.auth import HTTPBasicAuth  # or HTTPDigestAuth, or OAuth1, etc.
+from zeep.transports import Transport
+from zeep.wsse.username import UsernameToken
 
 import config
 
-#integration = name of SOAP integration to be used, contains 'url' and 'response' attributes
+#url = WSDL url
 #data = SOAPrequest parameters
 def send_request(url, data):
-    #get wsdl information from config.py file
-    #wsdl = config.wsdlList[integration]
     wsdlMethods = []
 
     #authentication and client initialization
@@ -35,11 +32,10 @@ def send_request(url, data):
                 wsdlMethods.append(operation.name)
                 signature = operation.input.signature()
 
-    #check if method signature contains specific parameters
     #client.service.[METHOD](<parameters>)
-    #getattr dynamically calls a method from the WSDL structure inspected above
+    #check if method signature contains specific parameters    
     if 'dataUltimoEnvio' in signature:
-        r = getattr(client.service, wsdlMethods[0])(req_data['dataUltimoEnvio'],req_data['ultimoRegistro'])
+        r = getattr(client.service, wsdlMethods[0])(req_data['dataUltimoEnvio'],req_data['ultimoRegistro']) #getattr dynamically calls a method from the WSDL structure inspected above
     elif 'ultimoRegistro' in signature:
         r = getattr(client.service, wsdlMethods[0])(req_data['ultimoRegistro'])
     else:
@@ -53,37 +49,37 @@ if input('Simple test [0] or Octopus Test? [1]: >') == '0':
     req_data = {'dataUltimoEnvio': '1999-01-01',
                 'ultimoRegistro': '0'}
 
-    r = send_request(config.wsdlList['localInstalacao'], req_data)
+    r = send_request(config.wsdlList['localInstalacao']['url'], req_data)
 
     print(r['localInstalacaoResponse'])
     input_dict = zeep.helpers.serialize_object(r)
     js = json.dumps(input_dict, indent=4)
     filename = 'localInstalacao-' + str(r['nRegistro']) + '.json'
-    arquivo = open(filename, 'a')
-    arquivo.write(js)
-    arquivo.close()
+    SOAPresponse = open(filename, 'a')
+    SOAPresponse.write(js)
+    SOAPresponse.close()
 
 # 1 = Full Octopus test
 else:
-    #config.octopusList contains multiples WSDL urls
-    for index, (name, values) in enumerate(config.wsdlList.items()):
+    #config.octopusList contains multiple WSDL urls
+    for index, (wsdlName, wsdlAttributes) in enumerate(config.wsdlList.items()):
         #SOAPrequest parameters
         req_data = {'dataUltimoEnvio': '1999-01-01',
                     'ultimoRegistro': '0'}
 
         while True:
-            r = send_request(values['url'], req_data)
+            r = send_request(wsdlAttributes['url'], req_data)
+
             if r != None:
                 print('nRegistro: ', r['nRegistro'])
                 print('Total Registros: ', r['totalRegistros'])
-                response = str([r[values['response']]])
 
                 input_dict = zeep.helpers.serialize_object(r)
                 js = json.dumps(input_dict, indent=4)
-                filename = 'results/' + str(index) + '-' + name + '-' + str(r['nRegistro']) + '.json'
-                arquivo = open(filename, 'a')
-                arquivo.write(js)
-                arquivo.close()
+                filename = 'results/' + str(index) + '-' + wsdlName + '-' + str(r['nRegistro']) + '.json'
+                SOAPresponse = open(filename, 'a')
+                SOAPresponse.write(js)
+                SOAPresponse.close()
 
                 req_data['ultimoRegistro'] = r['nRegistro']
 
